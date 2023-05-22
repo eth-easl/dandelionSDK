d6407dfb1f3a53d508388a96fee6940272aa04ae
Georgijs Vilums <georgijs.vilums@gmail.com>
2023-05-22 11:52:18 +0200

    fix missing system headers when compiling for cheri

--- system/platform/cheri/cheri.c
+++ system/platform/cheri/cheri.c
@@ -1,6 +1,5 @@
 #include "../../system.h"
-
-#include <stdint.h>
+#include <stddef.h>
 
 const void * __capability __dandelion_return_address;
 
@@ -16,6 +15,6 @@ void __dandelion_platform_exit(void) {
 }
 
 void __dandelion_platform_set_thread_pointer(void* ptr) {
-	uintptr_t thread_data = (uintptr_t)ptr;
+	size_t thread_data = (size_t)ptr;
 	asm volatile ("msr tpidr_el0, %0" :: "r"(thread_data));
 }
