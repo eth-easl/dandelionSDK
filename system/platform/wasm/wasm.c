c0189033dbe4da5662193240eb1a8c35f0443abf
leon-thomm <l.thomm@mailbox.org>
2023-10-13 21:38:52 +0200

    add platform target 'wasm'

---
+++ system/platform/wasm/wasm.c
@@ -0,0 +1,8 @@
+#include "../../system.h"
+#include <stddef.h>
+
+void __dandelion_platform_init(void) {}
+
+void __dandelion_platform_exit(void) {}
+
+void __dandelion_platform_set_thread_pointer(void* ptr) {}
