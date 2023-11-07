6fbd50b1d341b5cf67293f20a35b824f169a437a
leon-thomm <l.thomm@mailbox.org>
2023-11-07 20:53:01 +0100

    remove wasm sysdata region

--- system/platform/wasm/wasm.c
+++ system/platform/wasm/wasm.c
@@ -1,18 +1,8 @@
 #include "../../system.h"
 #include <stddef.h>
 
-// TODO: currently a hard-coded number of bytes for the interface memory
-const size_t  WASM_SYSDATA_REGION_SIZE = 12345;  // 16 KiB
-const char    WASM_SYSDATA_REGION[WASM_SYSDATA_REGION_SIZE] = {0};
+// TODO probably want to define SDK heap size in user code eventually
 const size_t  SDK_HEAP_SIZE = 1; // in wasm pages (one page is 64 KiB)
-
-size_t __wasm_sysdata_region_size(void) {
-    return WASM_SYSDATA_REGION_SIZE;
-}
-
-const char* __wasm_sysdata_region_base(void) {
-    return WASM_SYSDATA_REGION;
-}
 
 size_t __wasm_sdk_heap_size(void) {
     return SDK_HEAP_SIZE;
