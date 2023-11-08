0bd40f2f7ba4b4ccba0b9bf170e0088ec9654fb0
richardlee <18626685+richardlee159@users.noreply.github.com>
2023-11-08 21:43:53 +0100

    Avoid syscall in setting thread pointer for arm64

--- system/platform/debug/debug.c
+++ system/platform/debug/debug.c
@@ -121,12 +121,18 @@ void __dandelion_platform_init(void) {
 }
 
 void __dandelion_platform_exit(void) {
-    dump_global_data();
+	dump_global_data();
 	__syscall(SYS_exit_group, 0);
-    __builtin_unreachable();
+	__builtin_unreachable();
 }
 
 void __dandelion_platform_set_thread_pointer(void *ptr) {
-	long ret = __syscall(SYS_arch_prctl, ARCH_SET_FS, ptr);
-	(void)ret;
+#if defined(__x86_64__)
+	__syscall(SYS_arch_prctl, ARCH_SET_FS, ptr);
+#elif defined(__aarch64__)
+	size_t thread_data = (size_t)ptr;
+	asm volatile("msr tpidr_el0, %0" :: "r"(thread_data));
+#else
+#error "Missing architecture specific code."
+#endif
 }
