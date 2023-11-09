bb32e41c8421ca6d015e8bccc32dca0849ff0cc3
leon-thomm <l.thomm@mailbox.org>
2023-11-09 20:52:49 +0100

    remove SDK_HEAP_SIZE parameter from platform code

--- system/platform/wasm/wasm.c
+++ system/platform/wasm/wasm.c
@@ -1,13 +1,6 @@
 #include "../../system.h"
 #include <stddef.h>
 
-// TODO probably want to define SDK heap size in user code eventually
-const size_t  SDK_HEAP_SIZE = 1; // in wasm pages (one page is 64 KiB)
-
-size_t __wasm_sdk_heap_size(void) {
-    return SDK_HEAP_SIZE;
-}
-
 void __dandelion_platform_init(void) {}
 
 void __dandelion_platform_exit(void) {}
