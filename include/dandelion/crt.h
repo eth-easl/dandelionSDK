38c51bf3c6f6280ba045988a0aa6ceb574269c04
leon-thomm <l.thomm@mailbox.org>
2023-11-21 11:30:17 +0100

    add wasm platform entry return
    The wasm platform doesn't have a conventional exit() call, instead the entry point must return.

--- include/dandelion/crt.h
+++ include/dandelion/crt.h
@@ -11,6 +11,9 @@ void _start(void) __attribute__((naked));
 void _start(void) {
 #if defined(__x86_64__)
     __asm__("call __dandelion_entry\n");
+#elif defined(__wasm__)
+    __asm__("call __dandelion_entry\n");
+    __asm__("return\n");
 #elif defined(__aarch64__)
     __asm__("bl __dandelion_entry");
 #else
