cc65da1aef96e77d1d9e458d18951047f96605ae
Tom Kuchler <tom.kuchler@inf.ethz.ch>
2025-01-21 11:19:08 +0100

    Adding minimal test programs

---
+++ test_programs/libcpp/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+  // cout << "Hello World" << endl;
+  std::fstream fs;
+  fs.open("/stdio/stdout", std::fstream::out);
+  fs << " test print " << endl;
+  fs.close();
+
+  cout << "stdout print" << endl;
+
+  return 0;
+}
+
