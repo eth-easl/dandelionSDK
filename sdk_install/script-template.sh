d5a6e71dc2ead60bf526de29926418a6acb57073
Tom Kuchler <tom.kuchler@inf.ethz.ch>
2025-06-24 13:50:28 +0200

    Start changes for dev container

--- sdk_install/script-template.sh
+++ sdk_install/script-template.sh
@@ -2,14 +2,44 @@
 
 set -e
 
+CLANG_NAME="clang"
+DEFAULT_CLANG=false
+
+# option c sets the clang name to look for
+# option d makes it so the finished clang is installed as default clang, assuming there is no default clang yet
+while getopts "c:d" opt; do
+    case "$opt" in
+        c)
+        echo "Looking for clang at $OPTARG"
+        CLANG_NAME="$OPTARG"
+        ;;
+        d)
+        echo "Trying to create default clang"
+        DEFAULT_CLANG=true
+        ;;
+        ?)
+        echo "Unkown argument to scipt"
+        ;;
+    esac
+done
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-COMPILER_INCLUDES=$(clang -print-file-name=include)
+COMPILER_INCLUDES=$($CLANG_NAME -print-file-name=include)
 sed -i "s|COMPILER_INCLUDES|$COMPILER_INCLUDES|g" @DANDELION_TARGET@-clang.cfg
 
+CLANG_PATH="$(which $CLANG_NAME)"
+CLANG_DIR="$(dirname $CLANG_PATH)"
+
 if ! [ -f @DANDELION_TARGET@-clang ]; then
-    CLANG_PATH=$(which clang)
     cp $CLANG_PATH @DANDELION_TARGET@-clang
     ln -s @DANDELION_TARGET@-clang @DANDELION_TARGET@-clang++
+fi
+
+if [ $DEFAULT_CLANG = true ] && ! [[ -f $CLANG_DIR/clang ]]; then
+    cp "$CLANG_PATH" "$CLANG_DIR/clang"
+    ln -s "$CLANG_DIR/clang" "$CLANG_DIR/clang++"
+    cp @DANDELION_TARGET@-clang.cfg "$CLANG_DIR/clang.cfg" 
+    cp @DANDELION_TARGET@-clang++.cfg "$CLANG_DIR/clang++.cfg" 
 fi
