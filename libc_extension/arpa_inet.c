12a95df31e3de8e8479e83473df5055dc03a76f2
Lanzelot Moll <lanzelotmoll@gmail.com>
2026-03-09 12:52:05 +0100

    fix missing inet.h headers build errors

---
+++ libc_extension/arpa_inet.c
@@ -0,0 +1,32 @@
+#include <arpa/inet.h>
+#include <errno.h>
+
+in_addr_t inet_addr(const char *cp) {
+  (void)cp;
+  errno = ENOSYS;
+  return (in_addr_t)-1;
+}
+
+char *inet_ntoa(struct in_addr in) {
+  (void)in;
+  errno = ENOSYS;
+  return 0;
+}
+
+const char *inet_ntop(int af, const void *restrict src, char *restrict dst,
+                      socklen_t size) {
+  (void)af;
+  (void)src;
+  (void)dst;
+  (void)size;
+  errno = ENOSYS;
+  return 0;
+}
+
+int inet_pton(int af, const char *restrict src, void *restrict dst) {
+  (void)af;
+  (void)src;
+  (void)dst;
+  errno = ENOSYS;
+  return -1;
+}
