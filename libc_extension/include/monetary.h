2b02c2c7076308053413635cc67db11498ad1089
Lanzelot Moll <lanzelotmoll@gmail.com>
2026-03-09 20:01:32 +0100

    fix missing monetary.h headers build errors

---
+++ libc_extension/include/monetary.h
@@ -0,0 +1,15 @@
+#ifndef _MONETARY_H_
+#define _MONETARY_H_
+
+#include <sys/cdefs.h>
+#include <sys/types.h>
+#include <sys/_locale.h>
+
+__BEGIN_DECLS
+
+ssize_t strfmon(char *restrict, size_t, const char *restrict, ...);
+ssize_t strfmon_l(char *restrict, size_t, locale_t, const char *restrict, ...);
+
+__END_DECLS
+
+#endif /* _MONETARY_H_ */
---
+++ libc_extension/monetary.c
@@ -0,0 +1,20 @@
+#include <monetary.h>
+#include <errno.h>
+// https://man7.org/linux/man-pages/man0/monetary.h.0p.html
+ssize_t strfmon(char *restrict s, size_t max, const char *restrict format, ...) {
+  (void)s;
+  (void)max;
+  (void)format;
+  errno = ENOSYS;
+  return -1;
+}
+
+ssize_t strfmon_l(char *restrict s, size_t max, locale_t loc,
+                  const char *restrict format, ...) {
+  (void)s;
+  (void)max;
+  (void)loc;
+  (void)format;
+  errno = ENOSYS;
+  return -1;
+}
