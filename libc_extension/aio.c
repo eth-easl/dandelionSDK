1d2f2cb0893df284f47895b4767c50bee87bd50c
Lanzelot Moll <lanzelotmoll@gmail.com>
2026-03-09 12:33:39 +0100

    fix missing aio.h headers build errors

---
+++ libc_extension/aio.c
@@ -0,0 +1,62 @@
+#include <aio.h>
+#include <errno.h>
+
+
+// https://man7.org/linux/man-pages/man7/aio.7.html
+
+int aio_cancel(int fd, struct aiocb *aiocbp) {
+  (void)fd;
+  (void)aiocbp;
+  errno = ENOSYS;
+  return -1;
+}
+
+int aio_error(const struct aiocb *aiocbp) {
+  (void)aiocbp;
+  errno = ENOSYS;
+  return -1;
+}
+
+int aio_fsync(int op, struct aiocb *aiocbp) {
+  (void)op;
+  (void)aiocbp;
+  errno = ENOSYS;
+  return -1;
+}
+
+int aio_read(struct aiocb *aiocbp) {
+  (void)aiocbp;
+  errno = ENOSYS;
+  return -1;
+}
+
+ssize_t aio_return(struct aiocb *aiocbp) {
+  (void)aiocbp;
+  errno = ENOSYS;
+  return -1;
+}
+
+int aio_suspend(const struct aiocb *const aiocb_list[], int nitems,
+                const struct timespec *timeout) {
+  (void)aiocb_list;
+  (void)nitems;
+  (void)timeout;
+  errno = ENOSYS;
+  return -1;
+}
+
+int aio_write(struct aiocb *aiocbp) {
+  (void)aiocbp;
+  errno = ENOSYS;
+  return -1;
+}
+
+int lio_listio(int mode, struct aiocb *restrict const list[restrict], int nent,
+               struct sigevent *restrict sig) {
+  (void)mode;
+  (void)list;
+  (void)nent;
+  (void)sig;
+  errno = ENOSYS;
+  return -1;
+}
