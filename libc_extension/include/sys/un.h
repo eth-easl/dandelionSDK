749836425f29413c6b86d2009ef35e6fcdc551a9
Lanzelot Moll <lanzelotmoll@gmail.com>
2026-03-10 15:56:29 +0100

    un.h

---
+++ libc_extension/include/sys/un.h
@@ -0,0 +1,22 @@
+#ifndef _SYS_UN_H
+#define _SYS_UN_H
+
+#include <sys/socket.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct sockaddr_un {
+  sa_family_t sun_family;
+  /* Linux unix(7) documents sun_path as 108 bytes:
+   * https://man7.org/linux/man-pages/man7/unix.7.html
+   */
+  char sun_path[108];
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SYS_UN_H */
