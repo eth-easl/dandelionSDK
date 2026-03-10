6a09621f21add850727b925be57848f5039e0b6d
Lanzelot Moll <lanzelotmoll@gmail.com>
2026-03-10 13:14:21 +0100

    in.h, netinet_in.c, tcp.h

---
+++ libc_extension/include/netinet/tcp.h
@@ -0,0 +1,9 @@
+#ifndef _NETINET_TCP_H_
+#define _NETINET_TCP_H_
+
+/* Value matches glibc netinet/tcp.h:
+ * https://sources.debian.org/src/glibc/2.41-7/sysdeps/gnu/netinet/tcp.h/
+ */
+#define TCP_NODELAY 1
+
+#endif /* _NETINET_TCP_H_ */
